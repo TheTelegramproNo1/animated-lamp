ad7b795ef5c62062041440969c18bc35f1235aeb
TELEGRAM PRO <94740746+TheTelegramproNo1@users.noreply.github.com>
2021-12-29 14:03:13 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,17 +8,4 @@ BOT_TOKEN = '5069937975:AAGMcqS8SZSLzIGO6ChxS5oGBdw1FRQ92U0'
 LOG_CHANNEL = '-1001536980778'
 DATABASE_URL = 'mongodb+srv://Izazkhan:izazkhan@cluster0.hlltt.mongodb.net/project0?retryWrites=true&w=majority'
 AUTH_USERS = '1928904042'
-    COLORS = "white"
-
-    FONT_SIZES_NAME = ["Small", "Medium", "Large"]
-    FONT_SIZES = [30, 40, 50]
-    POSITIONS = [
-        "Top Left",
-        "Top Center",
-        "Top Right",
-        "Center Left",
-        "Centered",
-        "Center Right",
-        "Bottom Left",
-        "Bottom Center",
-        "Bottom Right"]
+    
