1cfae62687f1d7cc01ae82aa8f3a3af1c0c281e7
TELEGRAM PRO <94740746+TheTelegramproNo1@users.noreply.github.com>
2021-12-29 13:56:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class Config:
 API_HASH = '0336e890a385149db77821a136a7fce3'
 BOT_TOKEN = '5069937975:AAGMcqS8SZSLzIGO6ChxS5oGBdw1FRQ92U0'
 LOG_CHANNEL = '-1001536980778'
-DATABASE_URL ='mongodb+srv://Izazkhan:izazkhan@cluster0.hlltt.mongodb.net/project0?retryWrites=true&w=majority
+DATABASE_URL = 'mongodb+srv://Izazkhan:izazkhan@cluster0.hlltt.mongodb.net/project0?retryWrites=true&w=majority'
 AUTH_USERS = '1928904042'
     COLORS = [
         "white",
